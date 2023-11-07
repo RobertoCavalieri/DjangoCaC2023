3c54350822e7fbe84aff7c8a47fcc09a16ea5bde
Roberto <roberto.c.torrisi@hotmail.com>
2023-11-07 07:47:56 -0300

    Creacion tabla grupo

--- djangoProjectCaC/settings.py
+++ djangoProjectCaC/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "cac_2023_23655",
         "USER": "postgres",
-        "PASSWORD": "Morrowind1",
+        "PASSWORD": "codoacodo2023",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
