cd89b4e8a9770ed833096be65b3cf5f9e7df954f
Roberto Cavalieri <roberto.c.torrisi@hotmail.com>
2023-11-27 20:48:22 -0300

    Creacion tabla grupo

--- core/models.py
+++ core/models.py
@@ -1,8 +1,7 @@
-# models.py
-
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import Q
+from django.utils import timezone
 
 class Horario(models.Model):
     nombre = models.CharField(max_length=100)
@@ -13,20 +12,39 @@ class Horario(models.Model):
     def __str__(self):
         return self.nombre
 
-# Usando un modelo Proxy ya que no agrego nuevos campos
-# class Persona(User):
-#     class Meta:
-#         proxy = True
 
 class Persona(models.Model):
-    nombre = models.CharField(max_length=50, verbose_name='Nombre', )
+    nombre = models.CharField(max_length=50, verbose_name='Nombre')
     apellido = models.CharField(max_length=50, verbose_name='Apellido')
     mail = models.EmailField(max_length=150, verbose_name='Email')
     telefono = models.CharField(max_length=50, verbose_name='Teléfono', blank=True)
-    usuario = models.OneToOneField(User,on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def nombre_completo(self):
         return f"{self.nombre} {self.apellido}"
+
+    def encontrar_tiempos_libres(self, fecha_inicio, fecha_fin):
+        fecha_inicio = timezone.make_aware(fecha_inicio)
+        fecha_fin = timezone.make_aware(fecha_fin)
+
+        eventos = self.evento_set.filter(
+            Q(inicio__range=(fecha_inicio, fecha_fin)) | Q(fin__range=(fecha_inicio, fecha_fin))
+        )
+
+        intervalos_ocupados = [(evento.inicio, evento.fin) for evento in eventos]
+
+        intervalo_libre = [(fecha_inicio, fecha_fin)]
+
+        for ocupado_inicio, ocupado_fin in intervalos_ocupados:
+            nuevo_intervalo_libre = []
+            for libre_inicio, libre_fin in intervalo_libre:
+                if libre_inicio < ocupado_inicio:
+                    nuevo_intervalo_libre.append((libre_inicio, min(ocupado_inicio, libre_fin)))
+                if libre_fin > ocupado_fin:
+                    nuevo_intervalo_libre.append((max(ocupado_fin, libre_inicio), libre_fin))
+            intervalo_libre = nuevo_intervalo_libre
+
+        return intervalo_libre
 
     def __str__(self):
         return self.nombre_completo()
@@ -50,4 +68,3 @@ class Evento(models.Model):
 
     def __str__(self):
         return f"{self.nombre} {self.inicio}"
-
--- core/urls.py
+++ core/urls.py
@@ -4,12 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
-    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'),name='login'),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
-
     path('contacto/', views.contacto, name='contacto'),
-
     path('eventos/alta', views.alta_evento, name='eventos_alta'),
     path('personas/alta', views.alta_persona, name='personas_alta'),
     path('personas/listado', views.PersonaListView.as_view(), name='personas_listado'),
@@ -17,9 +14,7 @@ urlpatterns = [
     path('grupos/alta', views.GrupoCreateView.as_view(), name='grupos_alta'),
     path('grupos/listado', views.GrupoListView.as_view(), name='grupos_listado'),
     path('grupos/detalle/<int:id_grupo>', views.grupo_detalle, name='grupos_detalle'),
-    path('grupos/update/<int:pk>/', views.GrupoUpdateView.as_view(),name='grupos_modificar'),
-    path('grupos/delete/<int:pk>/', views.GrupoDeleteView.as_view(),name='grupos_eliminar'),
-    # path('grupos/calendario', views.calendario_grupo, name='Calendario de grupo'),
-    # path('cargar_horario', views.cargar_horario, name='cargar_horario'),
-
+    path('grupos/update/<int:pk>/', views.GrupoUpdateView.as_view(), name='grupos_modificar'),
+    path('grupos/delete/<int:pk>/', views.GrupoDeleteView.as_view(), name='grupos_eliminar'),
+    path('tiempos-libres/<int:persona_id>/', views.visualizar_tiempos_libres, name='tiempos_libres'),
 ]
